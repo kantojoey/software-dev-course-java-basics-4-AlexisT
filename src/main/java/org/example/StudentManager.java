9e46e64cc2a4defd608e85d2b87e69cd60452a5c
kantojoey <116197161+kantojoey@users.noreply.github.com>
2025-12-14 00:27:28 -0600

    Built student manager and successfully tested outcomes

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -35,7 +35,14 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.println("Please enter the name of the student you would like to add:");
+        String newStudent = scanner.nextLine();
+        if (!students.contains(newStudent)) {
+            students.add(newStudent);
+            System.out.println(newStudent + " has been successfully added!");
+        } else {
+            System.out.println("That student is already enrolled! Come talk to me when you mean business.");
+        }
         mainMenu();
     }
 
@@ -43,6 +50,14 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
+        System.out.println("Please enter the name of the student you would like to remove from the class");
+        String removed = scanner.nextLine();
+        if (students.contains(removed)) {
+            students.remove(removed);
+            System.out.println(removed + " was successfully removed from your class. I hope you're happy!");
+        } else {
+            System.out.println("That student was not found! Try again.");
+        }
 
         mainMenu();
     }
@@ -50,6 +65,16 @@ public class StudentManager {
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
+
+        if (students.size() > 0) {
+            for (String student : students) {
+                System.out.println("Here is your STUDENT LIST:");
+                System.out.println(student);
+                System.out.println("TA-DAAAAAA!!!");
+            }
+        } else {
+            System.out.println("SORRY BUDDY it looks like nobody wants to take your class. Why is that?");
+        }
         mainMenu();
     }
 }
